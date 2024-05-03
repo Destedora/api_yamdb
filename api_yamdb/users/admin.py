c5c87c0850c78ee157a8615840826e67a16f716a
Дарья Анохина <destedora-69@mail.ru>
2024-05-03 22:44:36 +0300

    admin in users

--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -1,10 +1,12 @@
-# users/admin.py
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
 from users.models import CustomUser
 
-UserAdmin.fieldsets += (
-    ('Extra Fields', {'fields': ('bio', 'role')}),
-)
-admin.site.register(CustomUser, UserAdmin)
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'role')
+    search_fields = ('username', 'email', 'first_name', 'last_name',
+                     'role', 'bio')
+    list_filter = ('role',)
