43d8ee9cdcff0ec82b3e082af079270cd3a49ca7
Дарья Анохина <destedora-69@mail.ru>
2024-05-03 23:33:11 +0300

    2.5

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -7,36 +7,30 @@ from rest_framework.permissions import (
 class IsAdminOrReadOnly(BasePermission):
     """
     Предоставляет полный доступ администратору,
-    остальным пользователям - только чтение
+    остальным пользователям - только чтение.
     """
 
     def has_permission(self, request, view):
         """
-        Проверяет, что пользователь является
-        администратором, либо метод безопасен
+        Проверяет, что пользователь является администратором,
+        либо метод безопасен.
         """
-        return (
-            request.method in SAFE_METHODS or (
-                request.user.is_authenticated and
-                request.user.is_admin
-            )
-        )
+        return (request.method in SAFE_METHODS
+                or request.user.is_authenticated and request.user.is_admin)
 
 
 class IsAdminModeratorAuthor(BasePermission):
     """
-    Предоставляет полуный доступ администратору, модератору
-    или автору, остальным пользователям - только чтение
+    Предоставляет полный доступ администратору, модератору,
+    автору, остальным пользователям - только чтение.
     """
 
     def has_object_permission(self, request, view, obj):
         """
         Проверяет, что пользователь является администратором,
-        модератором, автором контента, либо метод безопасен
+        модератором, автором контента, либо метод безопасен.
         """
-        return (
-            request.method in SAFE_METHODS or
-            obj.author == request.user or
-            request.user.is_admin or
-            request.user.is_moderator
-        )
+        return (request.method in SAFE_METHODS
+                or obj.author == request.user
+                or request.user.is_moderator
+                or request.user.is_admin)
